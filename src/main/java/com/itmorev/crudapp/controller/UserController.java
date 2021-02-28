76e86a47c55c740697a04b776f1c99cd75e73ea0
VadimMorev <77121381+VadimMorev@users.noreply.github.com>
2021-02-28 02:25:29 +0200

    Created UserController methods which shows all users and creates with adding. Created html page with buttons to create, update and delete user, html page with forms to enter user's name and surname.

---
+++ src/main/java/com/itmorev/crudapp/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.itmorev.crudapp.controller;
+
+import com.itmorev.crudapp.model.User;
+import com.itmorev.crudapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String findAll(Model model){
+        List<User> users=userService.findAll();
+        model.addAttribute("users",users);
+        return "user-list";
+    }
+    @GetMapping("user-create")
+    public String createUserForm(User user){
+
+        return "user-create";
+    }
+    @PostMapping("/user-create")
+    public String createUser(User user){
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+}
